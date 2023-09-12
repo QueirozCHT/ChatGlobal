2ecd88a8d718fd9b30a99064f56efb1c9f4988e7
QueirozCHT <mateusso475@gmail.com>
2023-09-12 20:32:44 -0300

    finalização

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,80 @@
+import { addDoc, collection, orderBy, limit, query, serverTimestamp } from "firebase/firestore";
+import { useState } from "react";
 import { useAuthState, useSignInWithGoogle } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
 import "./App.css";
-import { auth } from "./services/firebaseConfig";
+import { auth, databaseApp } from "./services/firebaseConfig";
+
 
 export const App = () => {
   const [user] = useAuthState(auth);
   return (
     <div className="App">
       <header>
-        <h1>ChatGlob</h1>
+        <h1>ChatGlobal</h1>
         <SignOut />
       </header>
       <section>{user ? <ChatRoom /> : <SignIn />}</section>
     </div>
-  );
+  );  
 };
 
 export const ChatRoom = () => {
-  return <h1> ChatRoom </h1>;
+  const messageRef = collection(databaseApp, "messages");
+  const QueryMessages = query(messageRef, orderBy("createdAt"), limit(50));
+  const [messages] = useCollectionData(QueryMessages, { idField: "id" });
+
+  const [formValue, setFormValue] = useState("");
+  const sendMessage = async (e) => {
+    e.preventDefault();
+
+    const { photoURL, uid } = auth.currentUser;
+
+    await addDoc(messageRef, {
+      text: formValue,
+      uid,
+      photoURL,
+      createdAt: serverTimestamp(),
+    }); 
+    setFormValue("");
+  };  
+  
+  return (
+    <>
+      <main>
+        {messages &&
+          messages.map((msg, index) => <ChatMessage key={index} message={msg} />)}
+      </main>
+      <form onSubmit={sendMessage}>
+        <input 
+          type="text" 
+          value={formValue} 
+          onChange={(e) => setFormValue(e.target.value)}
+        />
+        <button>Enviar</button>
+      </form>
+    </>
+  );
+};
+
+export const ChatMessage = (props) => {
+  const { text, photoURL, uid } = props.message;
+
+  const messageClass = uid === auth.currentUser.uid ? "sent" : "received"; 
+
+  return (
+    <div className={`message ${messageClass}`}>
+      <img src={photoURL} />
+        <p>{text}</p>
+    </div>
+  );
 };
 
 export const SignIn = () => {
   const [signInWithGoogle] = useSignInWithGoogle(auth);
   return (
     <button className="sign-in" onClick={() => signInWithGoogle()}>
-      Conta Google
+      Login with Google
     </button>
   );
 };
